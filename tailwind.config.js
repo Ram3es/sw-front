d779b7bd68dec13548d199e25f6be09c8e25ab31
Roman Luzan <romaluzan1@gmail.com>
2023-06-28 22:07:28 +0300

    added-bonus-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,27 @@ export default {
         sidebarGrey: '#202023',
         linkUnderline: '#6842FF',
         skinwalletPink: '#CFBBFE',
+        swBlack:'#0D0D0D',
+        swViolet: '#6842FF',
+        swGrey:'#A4A4A4',
+        'gray-42':'#424242'
       },
       screens: {
-        'sm-viewport': '50em'
+        'sm-viewport': '50em',
+        ssm: "350px",
+        ssl: "450px",
+        sm: "550px",
+        md: "700px",
+        lg: "900px", 
+        llg: "1100px",
+        xl: "1300px", 
+        xll: "1600px",
+        xxl: "1750px",
+        fourk: "2500px",
       },
+      fontFamily:{
+        Barlow: ['Barlow']
+      }
     },
   },
   plugins: [

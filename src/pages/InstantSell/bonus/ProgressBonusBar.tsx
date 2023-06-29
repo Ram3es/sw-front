6230cf90ab51c8699e322b58d00632633764801d
Roman Luzan <romaluzan1@gmail.com>
2023-06-29 12:21:05 +0300

    upd-scale

--- src/pages/InstantSell/bonus/ProgressBonusBar.tsx
+++ src/pages/InstantSell/bonus/ProgressBonusBar.tsx
@@ -7,14 +7,16 @@ const ProgressScale = () => {
             <div className='w-full h-5 flex justify-between px-10 sm:px-14'>
                 {BONUS_POINTS.map((item, idx) => <div key={item.percent} className={`w-[2px] h-full ${idx > 0 ? 'bg-gray-42' : 'bg-swViolet' } `} />)}
             </div>
-            <div className='w-full px-4 sm:px-8 absolute left-0 top-1/2 -translate-y-1/2'>
+            <div className='w-full px-10 sm:px-14  absolute  left-0 top-1/2 -translate-y-1/2'>
                 <div className='w-full h-1 relative bg-gray-42'>
                     <div 
                         className={` absolute inset-0 bg-linkUnderline`} 
-                        style={{ width:`${15}%`}}
+                        style={{ width:`${3}%`}}
                     />
                 </div>
             </div>
+            <div className='absolute h-1 w-5 ml-5 sm:ml-9 bg-linkUnderline left-0 top-1/2 -translate-y-1/2' />
+            <div className='absolute h-1 w-5 mr-5 sm:mr-9 bg-gray-42 right-0 top-1/2 -translate-y-1/2'/>
         </div>
     )
 }
@@ -31,7 +33,7 @@ const ProgressBonusBar = () => {
                             +{card.percent}%
                         </div>
                     </div>
-                    <div className={`mt-4 ${ card.percent < 3 ? 'text-white' : 'text-swGrey'} `}>${0}.00</div>
+                    <div className={`mt-4 ${ card.percent < 1 ? 'text-white' : 'text-swGrey'} `}>${0}.00</div>
                 </div>
             ))}
            <div className='w-full absolute left-0 bottom-[25%] sm:bottom-[30%] '>

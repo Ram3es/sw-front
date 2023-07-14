f00b87ecfe889c43ddc48ffc4b70eaf09d421f9d
Alexey Tsukanov <alexey.t@itsua.org>
2023-07-14 17:32:24 +0300

    remove base url from export

--- src/services/axios.instance.ts
+++ src/services/axios.instance.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-export const BASE_URL: string = import.meta.env.VITE_BASE_URL
 export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL
 
 export const GET = async <T>(endpoint: string): Promise<T> =>

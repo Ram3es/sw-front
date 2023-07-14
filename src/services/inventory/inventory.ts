d8365ed8b52228a6fb4ad16ea52f0db88c643d1b
Alexey Tsukanov <alexey.t@itsua.org>
2023-07-14 13:10:18 +0300

    small type fix

--- src/services/inventory/inventory.ts
+++ src/services/inventory/inventory.ts
@@ -1,6 +1,6 @@
 import { type SteamItem } from '../../types/Inventory'
 import { GET } from '../axios.instance'
 
-type TResponse = { default: SteamItem[] } & Record<string, SteamItem>
+type TResponse = Record<string, SteamItem>
 
 export const getInventory = async (gameId: string) => await GET<TResponse>(`/inventory?appid=${gameId}`)

1a75d5c67d666e2f9fd75ea4fefdac42d1615da3
Roman Luzan <romaluzan1@gmail.com>
2023-07-24 14:37:00 +0300

    added footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,7 +53,8 @@ export default {
       },
       gridTemplateColumns: {
         cards: "repeat(auto-fill,minmax(190px,1fr))",
-        categories:"repeat(auto-fill,minmax(150px, 1fr))"
+        categories:"repeat(auto-fill,minmax(150px, 1fr))",
+        footer:'repeat(auto-fill,minmax(150px, 1fr))'
       },
       boxShadow: {
         'checkedCard': '0px 0px 20px 0px rgba(0, 0, 0, 0.50)',

6a14cda6855527e37949a505c3702c4cf32bc555
Roman Luzan <romaluzan1@gmail.com>
2023-07-28 16:32:35 +0300

    added readme

--- src/pages/AddFunds/AddFunds.tsx
+++ src/pages/AddFunds/AddFunds.tsx
@@ -1,14 +1,32 @@
 import Bar from '../../components/Bar/Bar'
+import { ReactComponent as GiftIcon } from '../../assets/img/profile/gift-icon.svg'
+import { NavLink } from 'react-router-dom'
+import Readme from './readme/Readme'
 
 const AddFunds = () => {
   return (
-    <Bar>
-      <div className="flex justify-between items-center h-full px-6">
-        <h1 className='text-white font-["Barlow"] text-[21px] font-medium uppercase'>
-         Add Funds
-        </h1>
-      </div>
-    </Bar>
+    <>
+      <Bar>
+        <div className="flex justify-between items-center h-full px-6">
+          <h1 className='text-white font-["Barlow"] text-[21px] font-medium uppercase'>
+            Add Funds
+          </h1>
+        </div>
+      </Bar>
+      <header className='w-full flex items-center h-12 border-b border-darkGrey mt-5 px-6' >
+        <NavLink
+          to={'/'}
+          className='flex items-center gap-2.5 ml-auto text-sm text-graySecondary uppercase  hover:text-white button'>
+          <GiftIcon />
+          <span className='uppercase tracking-[1.12px]' >redeem gift card</span>
+        </NavLink>
+      </header>
+      <div className='w-full flex justify-center px-2 py-12'>
+          <div className='w-full max-w-[672px] flex flex-col gap-12'>
+            <Readme />
+          </div>
+        </div>
+    </>
   )
 }
 
---
+++ src/pages/AddFunds/readme/Readme.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Bullhorn from '../../../components/icons/Bullhorn'
+import { NavLink } from 'react-router-dom'
+import MinusIcon from '../../../components/icons/MinusIcon'
+import PlusIcon from '../../../components/icons/PlusIcon'
+
+const Readme = () => {
+  const [isOpen, setIsOpen] = useState(true)
+  return (
+        <div className='flex flex-col gap-4 p-6 bg-dark-14 text-graySecondary text-sm  '>
+            <div
+              onClick={() => { setIsOpen(boolean => !boolean) } }
+              className='w-full flex justify-between items-center group hover:text-white cursor-pointer duration-200 '
+            >
+                <div className='flex items-center gap-2 px-3 py-1 border group-hover:border-white  border-graySecondary  rounded-3xl'>
+                    <Bullhorn />
+                    <span className='uppercase  tracking-[1.12px]'>read me</span>
+                </div>
+                    {isOpen ? <MinusIcon /> : <PlusIcon /> }
+            </div>
+            {isOpen &&
+                <div className='flex flex-col gap-4 leading-5 [&>p]:font-normal' >
+                    <p>Wallet funds let you easily purchase items on Skinwallet Market without processing payments with every transaction. To keep the procedure safe and controllable, added funds cannot be paid back out, so take that into consideration before choosing a top-up amount.</p>
+                    <p>Bear in mind that every top-up attempt counts to the monthly top-up limit.</p>
+                    <p>Your top-up may require an additional KYC and/or AML verification. In such case, please follow the instructions displayed on the screen and wait for the verification process to finalize. The verification may take up to few hours.If, for any reason, your KYC procedure fails, your balance will be refunded.If, for any reason, your KYC procedure fails, your balance will be refunded.</p>
+                    <p>If, for any reason, your KYC procedure fails, your balance will be refunded.</p>
+                    <NavLink
+                      to={'/terms-of-service'}
+                      className='underline hover:no-underline'
+                    >
+                        Read more about Wallet in our Terms of Service
+                    </NavLink>
+                </div>
+            }
+        </div>
+  )
+}
+
+export default Readme

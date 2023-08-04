5cae297f459a655c454d80163ebeb8fc6ed9879b
Alexey Tsukanov <alexey.t@itsua.org>
2023-08-04 17:30:52 +0300

    small changes

--- src/services/market/market.ts
+++ src/services/market/market.ts
@@ -3,6 +3,6 @@ import { GET, POST } from '../axios.instance'
 
 export const getOffers = async (sortBy: string) => await GET<IOffersResponse>(`market/offers?appid=730&sortBy=${sortBy}`)
 
-export const getItemsToWithdraw = async () => await GET<any>('market/inventory?appid=730')
+export const getItemsToWithdraw = async () => await GET<any>('market/inventory')
 
 export const buyItems = async (params: { assetIds: string[] }) => await POST<string, typeof params>('/market/buy', params)

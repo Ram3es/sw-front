57f2a571189ab7147f6c3db0d7b943f309b763ab
Roman Luzan <romaluzan1@gmail.com>
2023-08-30 17:32:09 +0300

    icons/top-bar

--- src/containers/ListboxWrapper.tsx
+++ src/containers/ListboxWrapper.tsx
@@ -2,19 +2,16 @@
 import { Listbox } from '@headlessui/react'
 import Image from 'next/image'
 import { classNames } from '../helpers/className'
+import ChevronDown from '@/components/icons/ChevronDown'
 
 const ListBoxWrapper = ({ title, className, children, onChange }: { children: JSX.Element, title: string | ((open?: boolean) => JSX.Element), className?: string, onChange?: (value: string) => void }) => {
   return (
         <Listbox onChange={onChange}>
-            <Listbox.Button className={classNames(className ?? '', 'full flex gap-2 px-4 items-center text-graySecondary hover:text-white button tracking-[1.12px] uppercase')}>
+            <Listbox.Button className={classNames(className ?? '', 'full flex gap-2 px-4 items-center text-graySecondary hover:text-white button font-Barlow tracking-[1.12px] uppercase')}>
                 {({ open }) => (
                     <>
                         {typeof title === 'function' ? title(open) : title}
-                        <Image
-                            width={12}
-                            height={12}
-                            src="/chevron-down.svg"
-                            alt="chevron-down"
+                        <ChevronDown
                             className={classNames('fill-current h-[12px] w-[12px]', open ? 'rotate-180' : '')}
                         />
                     </>

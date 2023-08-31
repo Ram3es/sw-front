73c988edb6e556c2efe704fa78e07a863fba8ef5
Roman Luzan <romaluzan1@gmail.com>
2023-08-31 11:41:09 +0300

    upd-stagging

---
+++ src/app/wallet/fund-steps/CryptoAdress.tsx
@@ -0,0 +1,50 @@
+
+import InformationIcon from '../../../components/icons/InformationIcon'
+import { format } from '../../../helpers/numberFormater'
+import AddCoupon from '../../../components/funds/coupon/AddCoupon'
+import RoundedBTC from '@/components/icons/wallet/RoundedBTC'
+import CopyIcon from '@/components/icons/CopyIcon'
+
+const CryptoAdress = () => {
+  return (
+    <div className='flex flex-col gap-8'>
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col'>
+          <h3 className='tracking-[1.12px] text-graySecondary uppercase text-sm'>step 3/4</h3>
+          <h2 className='uppercase tracking-[1.28px] text-white '>TOP-UP WITH {'BITCOIN'}</h2>
+        </div>
+        <RoundedBTC />
+      </div>
+      <div className='flex flex-row w-full py-8 px-6 bg-dark-14 '>
+        <img src='img/funds/mock-QR-code.png' alt='qr-code' className='mr-6 hidden sm:block' />
+        <div className='w-full sm:w-[calc(100%_-_134px)] flex flex-col gap-3'>
+          <span className='text-lg text-graySecondary uppercase leading-[18px] tracking-[1.44px]' >DEPOSIT ADDRESS</span>
+          <div className='w-full h-14 flex items-center justify-between px-5 bg-darkGrey '>
+            <div className='text-white truncate w-full pr-4' >38wmw4uou2bWPEw4USzunSHdA9t7JhfvHXd</div>
+            <div className='text-graySecondary hover:text-white duration-200 cursor-pointer'>
+                <CopyIcon className='w-5 h-auto ' />
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-graySecondary '>
+            <InformationIcon iconClasses='w-4 h-4 shrink-0 ' />
+            <span className='text-sm leading-[14px] font-normal' >Only send BTC to the address above, 1 confirmation(s) required.</span>
+          </div>
+        </div>
+      </div>
+      <div className='w-max flex items-center gap-6 text-white'>
+        <div>
+            <span className='text-xs text-graySecondary font-normal block'>USD amount</span>
+            <span className='block'>${format(500)}</span>
+        </div>
+        <span>=</span>
+        <div>
+            <span className='text-xs text-graySecondary font-normal block'>Bitcoin amount</span>
+            <span className='block'>0.12598032098</span>
+        </div>
+      </div>
+      <AddCoupon />
+  </div>
+  )
+}
+
+export default CryptoAdress
---
+++ src/app/wallet/fund-steps/Summary.tsx
@@ -0,0 +1,166 @@
+import { useState } from 'react'
+import RoundedMark from '../../../components/icons/RoundedMark'
+import { useFundsContext } from '../../../context/FundsContext'
+import EditPencil from '../../../components/icons/EditPencil'
+import { format, formatToDecimal } from '../../../helpers/numberFormater'
+import { Button } from '../../../components/Navigation'
+import { classNames } from '../../../helpers/className'
+import CouponLabel from '../../../components/funds/coupon/CouponLabel'
+import InputWithErrors from '../../../components/Content/InputWithErrors'
+import { ImportantNotice, NoticeContent } from '../../../components/funds/ImportantNotice'
+import CloseIcon from '@/components/icons/CloseIcon'
+
+const Summary = () => {
+  const [isEditAmount, setIsEditAmount] = useState(false)
+  const [isEditCoupon, setIsEditCoupon] = useState(false)
+  const {
+    amountInputValue,
+    couponInputValue,
+    couponInfo,
+    selectedMethod,
+    errorsState,
+    isLoading,
+    setAddFundsStep,
+    setAmountInputValue,
+    setCouponInputValue,
+    setCouponInfo,
+    handleBlurInputAmount,
+    handleBlurInputCoupon
+  } = useFundsContext()
+
+  return (
+    <div className='w-full flex flex-grow justify-center pt-6 sm:py-12 '>
+        <div className='w-full h-[calc(100vh_-_234px)] sm:h-full max-w-[1024px] flex flex-col justify-between sm:flex-row sm:gap-8 '>
+          <div className='w-full  h-full max-w-[672px] flex flex-col gap-4 pb-10 px-6 sm:px-0 text-white overflow-y-scroll relative '>
+            <div className='w-full flex flex-col gap-y-3 sm:flex-row items-start px-6 py-4 bg-darkGrey relative'>
+              <div className='w-full sm:w-1/3 flex items-center gap-2'>
+                <RoundedMark className='text-swLime w-[18px] h-auto shrink-0 ' />
+                <span className='text-lg text-graySecondary uppercase tracking-[1.44px]' >method</span>
+              </div>
+              <div className='flex flex-col text-sm font-normal text-graySecondary'>
+                <h3 className='text-white text-lg font-medium mb-4'>{selectedMethod?.title}</h3>
+                <NoticeContent className='hidden sm:flex' />
+                <ImportantNotice className='sm:hidden' />
+              </div>
+              <div
+                onClick={() => { setAddFundsStep(1) } }
+                className='absolute top-4 right-6 text-graySecondary group cursor-pointer '
+                >
+                 <EditPencil className='group-hover:text-white duration-200' />
+              </div>
+            </div>
+            {isEditAmount
+              ? (
+                <InputWithErrors
+                  value={amountInputValue}
+                  handleChange={(value: string) => { setAmountInputValue(value) }}
+                  onClear={() => { setAmountInputValue('') } }
+                  handleBlur={handleBlurInputAmount}
+                  error={Object.values(errorsState).filter(obj => obj.status && obj.relative === 'amount')[0]}
+                  errorBorder='border-swOrange'
+                  autoFocus
+               />
+                )
+              : (
+                 <div className='w-full flex flex-col sm:flex-row items-start px-6 py-4 bg-darkGrey relative'>
+                  <div className='w-full sm:w-1/3 flex items-center gap-2'>
+                    <RoundedMark className='text-swLime w-[18px] h-auto ' />
+                    <span className='text-lg text-graySecondary uppercase tracking-[1.44px]' >amount</span>
+                  </div>
+                  <div className='w-full sm:w-2/3 flex items-center justify-between'>
+                    <span className='text-lg text-white'>${amountInputValue}</span>
+                    <div
+                      onClick={() => { setIsEditAmount(true) } }
+                      className='text-graySecondary group cursor-pointer '
+                    >
+                      <EditPencil className='group-hover:text-white duration-200 ' />
+                    </div>
+                  </div>
+                </div>)
+            }
+            { couponInfo || isEditCoupon
+              ? !isEditCoupon
+                  ? (
+                <div className='w-full flex flex-col sm:flex-row items-start px-6 py-4 bg-darkGrey relative'>
+                <div className='w-full sm:w-1/3 flex items-center gap-2'>
+                  <RoundedMark className='text-swLime w-[18px] h-auto' />
+                  <span className='text-lg text-graySecondary uppercase tracking-[1.44px]' >coupon code</span>
+                </div>
+                <div className='w-full sm:w-2/3 flex items-center justify-between'>
+                  <span className='text-lg text-white'>Awesome 2000 code</span>
+                  <div className='flex items-center gap-4'>
+                    <div
+                      onClick={() => { setIsEditCoupon(true) } }
+                      className='text-graySecondary group cursor-pointer '
+                    >
+                      <EditPencil className='group-hover:text-white duration-200 ' />
+                    </div>
+                    <div
+                      onClick={() => { setCouponInfo(0); setCouponInputValue('') }}
+                      className='text-graySecondary hover:text-white duration-200 cursor-pointer'
+                    >
+                      <CloseIcon className='w-3 h-[18px] ' />
+                    </div>
+                  </div>
+                </div>
+              </div>
+                    )
+                  : (
+                    <InputWithErrors
+                      label='coupon code'
+                      value={couponInputValue}
+                      handleChange={(value) => { setCouponInputValue(value) }}
+                      onClear={() => { setCouponInputValue('') }}
+                      handleBlur={() => { void handleBlurInputCoupon() }}
+                      errorBorder='border-swRed'
+                      error={errorsState.wrongCoupon }
+                      isLoading={isLoading}
+                      autoFocus
+                      variant='coupon'
+                    />
+                    )
+              : <CouponLabel submitFn={() => { setIsEditCoupon(true) }} />}
+          </div>
+          <div className=' h-max w-full sm:w-[320px] text-graySecondary bg-darkGrey p-6 sm:cta-clip-path relative'>
+            <div className='w-full '>
+              <div className='flex flex-col gap-2'>
+                <div className='w-full flex justify-between items-center text-sm '>
+                  <div className='uppercase tracking-[1.12px]'>amount</div>
+                  <span className='text-white'>${formatToDecimal(amountInputValue)}</span>
+                </div>
+                {!!couponInfo &&
+                  <div className='flex flex-col gap-2 pb-6 mb-4 border-b border-white/10'>
+                    <div className='w-full flex justify-between items-center text-sm '>
+                      <div className='uppercase tracking-[1.12px]'>active coupon</div>
+                      <span className='text-swLime'>+${formatToDecimal(couponInfo.toString())}</span>
+                    </div>
+                    <div className='w-full flex justify-between items-center text-sm '>
+                      <div className='uppercase tracking-[1.12px]'>final top-up</div>
+                      <span >${formatToDecimal((couponInfo + +amountInputValue).toString())}</span>
+                    </div>
+                  </div>
+                    }
+                <div className='w-full flex justify-between items-center text-sm '>
+                  <div className='uppercase tracking-[1.12px]'>payment fee</div>
+                  <span className=''>${format(0)}</span>
+                </div>
+                <div className='w-full flex justify-between items-center text-sm '>
+                  <div className='uppercase tracking-[1.12px]'>total payment</div>
+                  <span className=' text-2xl leading-6  text-white '>${formatToDecimal(amountInputValue)}</span>
+                </div>
+              </div>
+            </div>
+            <Button
+              text='proceed payment'
+              onClick={() => {}}
+              className={classNames('bg-skinwalletPink justify-center items-center w-full h-[48px] uppercase text-dark-14 hover:opacity-50 duration-200  ml-auto mt-12 cta-clip-path',
+                selectedMethod?.methodName ? '' : 'pointer-events-none grayscale opacity-50')}
+             />
+             <div className='w-full absolute left-0 -top-10 h-10 sm:hidden' style={{ background: 'linear-gradient(180deg, rgba(20, 20, 21, 0.00) 0%, #0D0D0D 100%)' }}/>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Summary

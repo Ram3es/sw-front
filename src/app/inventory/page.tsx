e2fda23644c4fa70d57f57717bc6db8e74b729de
Andrey <andrey@itsua.org>
2023-08-31 15:55:57 +0300

    some pages moved to next

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -4,9 +4,9 @@ export default function Inventory() {
   return (
     <Bar>
       <div className="flex justify-between items-center h-full px-6">
-      <h1 className='text-white font-Barlow text-[21px] font-medium uppercase'>
-        Inventory
-      </h1>
+        <h1 className='text-white font-Barlow text-[21px] font-medium uppercase'>
+          Inventory
+        </h1>
       </div>
     </Bar>
   )

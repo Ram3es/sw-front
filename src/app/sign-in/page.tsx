c35edd14fe89958494a5db64a61e5129a4298b49
Andrey <andrey@itsua.org>
2023-09-04 17:34:38 +0300

    build checked and fixed

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link"
 import SteamIcon from '../../components/icons/SteamIcon'
 import { useAppContext } from '../../context/AppContext'
 import { redirect } from 'next/navigation';
-import { NextPageContext } from "next"
 import { URLS } from "@/constants/common";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
-export default function Signin(ctx: NextPageContext & { searchParams: any }) {
-  const { searchParams } = ctx;
-  const continueUrl = searchParams?.continueUrl || '/'
+export default function Signin() {
+  const searchParams = useSearchParams()
+  const continueUrl = searchParams.get('continueUrl')
   const { user } = useAppContext()
   const [isClient, setIsClient] = useState(false);
 

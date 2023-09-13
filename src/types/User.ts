e864a88b5890f8591e7746f94839d6ff67e99497
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-09-13 13:16:17 +0300

    add new field in iuser

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,24 +1,37 @@
+export interface IBillingAddress {
+  firstName: string
+  lastName: string
+  streetAddress: string
+  streetAddress2: string
+  city: string
+  province: string
+  zip: string
+  country: string
+}
+
 export interface IUser {
   id: string
   username: string
   balance: number
   payoutLimit?: number
   avatar: string
+  billingAddress?: IBillingAddress
 }
 
 export interface ISteamUser {
-  id: number;
-  steamId: string;
-  steamUsername: string;
-  avatarUrl: string;
-  profileUrl: string;
-  payout: number;
-  banned: number;
-  balance: number;
-  transactionsTotal: number;
-  tradeUrl?: string | null;
-  notifications: number;
-  active: number;
-  email?: string | null;
-  createdAt: string;
+  id: number
+  steamId: string
+  steamUsername: string
+  avatarUrl: string
+  profileUrl: string
+  payout: number
+  banned: number
+  balance: number
+  transactionsTotal: number
+  tradeUrl?: string | null
+  notifications: number
+  active: number
+  email?: string | null
+  createdAt: string
+  billingAddress?: IBillingAddress
 }
